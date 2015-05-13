7d634ad8066e037684080d9e428a776a43be0ab6
NerdyItalian <atava8384@gmail.com>
2015-05-13 16:59:38 -0500

    working calculator with styling, initial ask

---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    var numLength = function(number) {
+        if (number.length > 9) {
+            total.text(number.substr(number.length-9,9));
+            if (number.length > 15) {
+                number = "";
+                total.text("Err");
+            }
+        }
+    };
+
+    var number = "";
+    var newNumber = "";
+    var operator = "";
+    var total = $("#total");
+    total.text("0");
+
+    $("#numbers a").not("#clear,#clearAll").click(function(){
+        number += $(this).text();
+        total.text(number);
+        numLength(number);
+    });
+    $("#operators a").not("#equals").click(function(){
+        operator = $(this).text();
+        newNumber = number;
+        number = "";
+        total.text("0");
+    });
+    $("#clear, #clearAll").click(function(){
+        number = "";
+        total.text("0");
+        if ($(this).attr("id") === "clearAll"){
+            newNumber = "";
+        }
+    });
+    //Add your last .click() here!
+    $("#equals").click(function(){
+        if (operator === "+"){
+            number = (parseInt(number, 10) + parseInt(newNumber,10)).toString(10);
+        } else if (operator === "-"){
+            number = (parseInt(newNumber, 10) - parseInt(number,10)).toString(10);
+        } else if (operator === "/"){
+            number = (parseInt(newNumber, 10) / parseInt(number,10)).toString(10);
+        } else if (operator === "*"){
+            number = (parseInt(newNumber, 10) * parseInt(number,10)).toString(10);
+        }
+        total.text(number);
+        numLength(number);
+        number = "";
+        newNumber = "";
+    });
+});
